29f98d13083e9bf8ffec7ade1ca5b543f53125fb
RichForever <mateuszmisiak1@gmail.com>
2024-12-30 15:48:00 +0100

    add github button

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	extends: ['@wordpress/stylelint-config'],
+	ignoreFiles: ['build/**/*.css', 'node_modules/**/*.css'],
+	rules: {
+		// Add any custom rules here
+	},
+};
--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1841b706de710b070826');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b44ca9647d8cf526ed40');
