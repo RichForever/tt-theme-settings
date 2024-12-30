b8e1a6b31231a40c7f36d78747539d88e28d2c11
RichForever <mateuszmisiak1@gmail.com>
2024-12-30 15:17:17 +0100

    test2

--- .stylelintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: ['@wordpress/stylelint-config'],
-    ignoreFiles: ['build/**/*.css', 'node_modules/**/*.css'],
-    rules: {
-        // Add any custom rules here
-    }
-};
