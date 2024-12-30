33d4ee7bf73a781cfcb32aa3324b59e0e01c5631
RichForever <mateuszmisiak1@gmail.com>
2024-12-30 15:07:07 +0100

    test

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -1,25 +1,25 @@
 /* eslint-disable prettier/prettier */
 import {
-  // eslint-disable-next-line @wordpress/no-unsafe-wp-apis
-  __experimentalVStack as VStack,
-} from "@wordpress/components";
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalVStack as VStack,
+} from '@wordpress/components';
 
-import FormSectionHeading from "@components/common/FormSectionHeading";
+import FormSectionHeading from '@components/common/FormSectionHeading';
 
-import styles from "@styles/Section.module.scss";
+import styles from '@styles/Section.module.scss';
 
 const Section = ({ sectionTitle, sectionDescription, children }) => {
-  return (
-    <div className={styles.section}>
-      <VStack spacing={4}>
-        <FormSectionHeading
-          title={sectionTitle}
-          description={sectionDescription}
-        />
-        {children}
-      </VStack>
-    </div>
-  );
+	return (
+		<div className={styles.section}>
+			<VStack spacing={4}>
+				<FormSectionHeading
+					title={sectionTitle}
+					description={sectionDescription}
+				/>
+				{children}
+			</VStack>
+		</div>
+	);
 };
 
 export default Section;
