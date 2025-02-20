95a1adab1f28c90326e54ee97b8686c80cba5ca1
RichForever <mateuszmisiak1@gmail.com>
2025-02-20 10:26:22 +0100

    feat: add release it changelog generation

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1bc6ba7d6979b04413d6');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1bd1353a457418ccfddb');
